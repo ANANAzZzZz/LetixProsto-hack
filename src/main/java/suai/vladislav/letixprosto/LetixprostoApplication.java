2415c80bf557982f55fde826e37306d75ec37a6c
ANANAzZzZz <vladscooltrol@gmail.com>
2024-04-20 11:29:27 +0300

    initial commit

---
+++ src/main/java/suai/vladislav/letixprosto/LetixprostoApplication.java
@@ -0,0 +1,13 @@
+package suai.vladislav.letixprosto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetixprostoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetixprostoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/suai/vladislav/letixprosto/LetixprostoApplicationTests.java
@@ -0,0 +1,13 @@
+package suai.vladislav.letixprosto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetixprostoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
