e364903fd3c1fbd99e1dc789801c5b332a8592e5
ANANAzZzZz <vladscooltrol@gmail.com>
2024-04-21 11:11:07 +0300

    add search by Vacancy competencies

--- src/main/java/suai/vladislav/letixprosto/controllers/ResponseVacancyController.java
+++ src/main/java/suai/vladislav/letixprosto/controllers/ResponseVacancyController.java
@@ -4,20 +4,50 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import suai.vladislav.letixprosto.Util;
-import suai.vladislav.letixprosto.models.User;
 import suai.vladislav.letixprosto.models.vacancy.ResponseVacancy;
+import suai.vladislav.letixprosto.models.vacancy.ResultsItem;
 import suai.vladislav.letixprosto.services.ResponseVacancyService;
-import suai.vladislav.letixprosto.services.UserService;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 @RequiredArgsConstructor
 public class ResponseVacancyController {
     private final ResponseVacancyService responseVacancyService;
+
+    @GetMapping("/searchByVacancy")
+    public ArrayList<ResultsItem> searchByVacancy(@RequestParam(value = "Competencies", defaultValue = "") String CompetenciesName) {
+        ArrayList<ResponseVacancy> serviceResponseList = responseVacancyService.getResponse();
+
+        Set<String> comptenciesSet = new HashSet<>(Arrays.asList(CompetenciesName.split(";")));
+    
+        ResultsItem match;
+
+        ArrayList<ResultsItem> ans = new ArrayList<>();
+        for (ResponseVacancy rv : serviceResponseList) {
+            for (int i = 0; i < rv.getResults().size(); i++) {
+                match = rv.getResults().get(i);
+
+                Set<String> tempSet =  new HashSet<>(comptenciesSet);;
+
+                for (int j = 0; j < match.getMainVacancyCompetencies().size(); j++) {
+                    tempSet.remove(match.getMainVacancyCompetencies().get(j).getName());
+                }
+
+                if (tempSet.isEmpty()) {
+                    ans.add(match);
+                }
+            }
+        }
+        return ans;
+    }
 
     @GetMapping("/getVacancy")
     public ArrayList<ResponseVacancy> getVacancy() {
--- src/main/java/suai/vladislav/letixprosto/services/ResponseVacancyService.java
+++ src/main/java/suai/vladislav/letixprosto/services/ResponseVacancyService.java
@@ -2,8 +2,6 @@ package suai.vladislav.letixprosto.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import suai.vladislav.letixprosto.models.User;
-import suai.vladislav.letixprosto.models.events.Response;
 import suai.vladislav.letixprosto.models.vacancy.ResponseVacancy;
 import suai.vladislav.letixprosto.repositories.ResponseVacancyRepository;
 
@@ -14,6 +12,15 @@ public class ResponseVacancyService {
     @Autowired
     private ResponseVacancyRepository responseVacancyRepository;
 
-    public void saveResponse(ResponseVacancy responseVacancy) { responseVacancyRepository.save(responseVacancy); }
-    public ArrayList<ResponseVacancy> getResponse() { return  (ArrayList<ResponseVacancy>) responseVacancyRepository.findAll(); }
+    public void saveResponse(ResponseVacancy responseVacancy) {
+        responseVacancyRepository.save(responseVacancy);
+    }
+
+    public ArrayList<ResponseVacancy> getResponse() {
+        return (ArrayList<ResponseVacancy>) responseVacancyRepository.findAll();
+    }
+
+    public ArrayList<ResponseVacancy> searchByMainVacancyCompetencies(String Competencies) {
+        return (ArrayList<ResponseVacancy>) responseVacancyRepository.findByResultsMainVacancyCompetenciesName(Competencies);
+    }
 }
